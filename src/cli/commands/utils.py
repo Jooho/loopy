b31acc8e8a001b1e29fa968a9f950a50734b8724
jooho <jlee@redhat.com>
2024-03-11 16:47:45 -0400

    remove double quotation if it is the value include it in the input file

--- src/cli/commands/utils.py
+++ src/cli/commands/utils.py
@@ -9,6 +9,7 @@ ROLE_SCHEMA_FILE_PATH = "./src/schema/role.yaml"
 UNIT_SCHEMA_FILE_PATH = "./src/schema/unit.yaml"
 PLAYBOOK_SCHEMA_FILE_PATH = "./src/schema/playbook.yaml"
 
+
 def initialize(directory, type):
     item_list = []
 
@@ -16,17 +17,17 @@ def initialize(directory, type):
         if "config.yaml" in files:
             config_path = os.path.join(root, "config.yaml")
 
-            file_errors = validate_role_yaml(config_path,type)
+            file_errors = validate_role_yaml(config_path, type)
             if file_errors:
                 for error in file_errors:
                     print(f"{Fore.RED}YAML Schema Error!{Style.RESET_ALL}")
                     print(f"{Fore.RED}ERROR: {error}{Style.RESET_ALL}")
                     print(f"{Fore.BLUE}YAML Content({config_path}){Style.RESET_ALL}")
-                    exit(1)      
-           
+                    exit(1)
+
             with open(config_path, "r") as config_file:
                 config_data = yaml.safe_load(config_file)
-                
+
                 if config_data:
                     if type == "unit":
                         path = os.path.abspath(root)
@@ -38,9 +39,7 @@ def initialize(directory, type):
                             role_name = config_data[type]["steps"][0]["role"]["name"]
                         else:
                             role_name = config_data[type]["role"]["name"]
-                        item_list.append(
-                            {"name": name, "path": path, "role_name": role_name}
-                        )
+                        item_list.append({"name": name, "path": path, "role_name": role_name})
                     else:
                         path = os.path.abspath(root)
                         if "name" in config_data[type]:
@@ -61,8 +60,10 @@ def convert_path_to_component_name(path, component):
     else:
         return None
 
+
 def get_default_vars(ctx):
     return ctx.obj.get("config", "default_vars")["default_vars"]
+
 
 def parse_key_value_pairs(ctx, param, value):
     if value is None:
@@ -70,17 +71,18 @@ def parse_key_value_pairs(ctx, param, value):
 
     result = {}
     for item in value:
-        if item.count('=') >= 2:
+        if item.count("=") >= 2:
             value_str = str(item)
             first_eq_index = value_str.find("=")
             key = value_str[:first_eq_index]
-            val = value_str[first_eq_index + 1:]
+            val = value_str[first_eq_index + 1 :]
             result[key] = val
         else:
             key, value = item.split("=")
             result[key] = value
-    
+
     return result
+
 
 def load_env_file_if_exist(file):
     additional_vars = {}
@@ -89,8 +91,10 @@ def load_env_file_if_exist(file):
             with open(file, "r") as file:
                 for line in file:
                     key, value = line.strip().split("=")
-                    # os.environ[key] = value
-                    additional_vars[key] = value
+                    if '"' in value:
+                        additional_vars[key] = value.replace('"', "")
+                    else:
+                        additional_vars[key] = value
         else:
             print(f"File({file}) does not exist")
             exit(1)
@@ -104,6 +108,7 @@ def set_env_vars_if_file_exist(file):
                     key, value = line.strip().split("=")
                     os.environ[key] = value
 
+
 def update_params_with_input_file(additional_vars_from_file, params):
     updated_params = params
     if len(additional_vars_from_file) != 0:
@@ -112,15 +117,15 @@ def update_params_with_input_file(additional_vars_from_file, params):
         updated_params = additional_vars_from_file
     return updated_params
 
+
 def get_config_data_by_config_file_dir(ctx, config_file_dir):
     try:
         with open(os.path.join(config_file_dir, "config.yaml"), "r") as config_file:
             config_data = yaml.safe_load(config_file)
             return config_data
     except FileNotFoundError:
-        ctx.invoke(
-            click.echo(f"Config file '{config_file_dir}/config.yaml' not found.")
-        )
+        ctx.invoke(click.echo(f"Config file '{config_file_dir}/config.yaml' not found."))
+
 
 def get_config_data_by_name(ctx, name, type, list):
     if type == "unit":
@@ -133,17 +138,20 @@ def get_config_data_by_name(ctx, name, type, list):
                 path = role["path"]
     return get_config_data_by_config_file_dir(ctx, path)
 
+
 def get_input_env_from_config_data(role_config_data):
     if "input_env" not in role_config_data:
         return None
     else:
         return role_config_data["input_env"]
 
+
 def get_input_env_from_unit_config_data(unit_config_data):
     if "input_env" not in unit_config_data:
         return None
     else:
         return unit_config_data["input_env"]
+
 
 def get_first_role_name_in_unit_by_unit_name(unit_name, list):
     for unit in list:
@@ -152,35 +160,32 @@ def get_first_role_name_in_unit_by_unit_name(unit_name, list):
 
 
 def validate_role_yaml(yaml_file, type):
-    schema=None
-    schema_file_path=None
+    schema = None
+    schema_file_path = None
     # Load schema file
-    if type == "role":        
-       schema_file_path=ROLE_SCHEMA_FILE_PATH
-    elif type == "unit":        
-        schema_file_path=UNIT_SCHEMA_FILE_PATH
-    elif type == "playbook": 
-        schema_file_path=PLAYBOOK_SCHEMA_FILE_PATH       
+    if type == "role":
+        schema_file_path = ROLE_SCHEMA_FILE_PATH
+    elif type == "unit":
+        schema_file_path = UNIT_SCHEMA_FILE_PATH
+    elif type == "playbook":
+        schema_file_path = PLAYBOOK_SCHEMA_FILE_PATH
 
     with open(schema_file_path, "r") as schema_file:
         schema = yaml.safe_load(schema_file)
-        
-    # Load Target yaml file 
+
+    # Load Target yaml file
     try:
         with open(yaml_file, "r") as f:
             yaml_data = yaml.safe_load(f)
     except yaml.YAMLError as e:
         print(f"{Fore.RED}YAML File Syntax Error:{Style.RESET_ALL}", e)
         exit(1)
-        
+
     # Create validator
     validator = Draft7Validator(schema)
 
     # Validate YAML Data
     errors = []
     for error in validator.iter_errors(yaml_data):
-        errors.append({
-            "message": error.message,
-            "path": list(error.path)
-        })    
+        errors.append({"message": error.message, "path": list(error.path)})
     return errors

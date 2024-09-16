4874366b96697d445dca1569faebe414f061fc21
jooho lee <jlee@redhat.com>
2024-09-16 11:25:21 -0400

    Rosa installation role
    Signed-off-by: jooho lee <jlee@redhat.com>

--- commons/scripts/utils.sh
+++ commons/scripts/utils.sh
@@ -21,34 +21,34 @@ die() {
   color_red='\e[31m'
   color_yellow='\e[33m'
   color_reset='\e[0m'
-  printf "${color_red}FATAL:${color_yellow} $*${color_reset}\n" 1>&2
+  printf "${color_red}FATAL:${color_yellow} $*${color_reset}\n"
   exit 1
 }
 
-info() {
+light_info() {
   color_blue='\e[34m'
   color_reset='\e[0m'
-  printf "${color_blue}$*${color_reset}\n" 1>&2
+  printf "${color_blue}$*${color_reset}\n"
 }
 
-light_info() {
+info() {
   color_reset='\e[0m'
-  printf "${cyan}$*${color_reset}\n" 1>&2
+  printf "${cyan}$*${color_reset}\n"
 }
 
 error() {
   color_reset='\e[0m'
-  printf "${error}$*${color_reset}\n" 1>&2
+  printf "${error}$*${color_reset}\n"
 }
 
 success() {
   color_green='\e[32m'
   color_reset='\e[0m'
-  printf "${color_green}$*${color_reset}\n" 1>&2
+  printf "${color_green}$*${color_reset}\n"
 }
 
-pending(){
-  printf "${pending}$*${color_reset}\n" 1>&2
+pending() {
+  printf "${pending}$*${color_reset}\n"
 }
 
 check_pod_status() {
@@ -58,7 +58,7 @@ check_pod_status() {
   local pod_status
   local pod_entry
 
-  pod_status=$(oc get pods -l $pod_selector -n $pod_namespace -o jsonpath="$JSONPATH") 
+  pod_status=$(oc get pods -l $pod_selector -n $pod_namespace -o jsonpath="$JSONPATH")
   oc_exit_code=$? # capture the exit code instead of failing
 
   if [[ $oc_exit_code -ne 0 ]]; then
@@ -88,41 +88,38 @@ check_pod_status() {
   return 0
 }
 
-function wait_pod_containers_ready(){
-    pod_label=$1
-    namespace=$2
-    checkcount=20
-    tempcount=0
-    while true; do
-        pod_exist=$(oc get pod -l ${pod_label} -n ${namespace} --ignore-not-found)
+function wait_pod_containers_ready() {
+  pod_label=$1
+  namespace=$2
+  checkcount=20
+  tempcount=0
+  while true; do
+    pod_exist=$(oc get pod -l ${pod_label} -n ${namespace} --ignore-not-found)
 
-        if [[ ${pod_exist} != '' ]]
-        then
-            ready=$(oc get pod -l ${pod_label} -n ${namespace} --no-headers|head -1|awk '{print $2}'|cut -d/ -f1)
-            desired=$(oc get pod -l ${pod_label} -n ${namespace} --no-headers|head -1|awk '{print $2}'|cut -d/ -f2)
+    if [[ ${pod_exist} != '' ]]; then
+      ready=$(oc get pod -l ${pod_label} -n ${namespace} --no-headers | head -1 | awk '{print $2}' | cut -d/ -f1)
+      desired=$(oc get pod -l ${pod_label} -n ${namespace} --no-headers | head -1 | awk '{print $2}' | cut -d/ -f2)
 
-            if [[ $ready == $desired ]]
-            then
-                success "[SUCCESS] Pod(s) with label '${pod_label}' is(are) Ready!"
-                break
-            else 
-                pending "[PENDING] Pod(s) with label '${pod_label}' is(are) ${red}NOT${clear}${pending} Ready yet: $tempcount times"
-                pending "[PENDING] Wait for 10 seconds"
+      if [[ $ready == $desired ]]; then
+        success "[SUCCESS] Pod(s) with label '${pod_label}' is(are) Ready!"
+        break
+      else
+        pending "[PENDING] Pod(s) with label '${pod_label}' is(are) ${red}NOT${clear}${pending} Ready yet: $tempcount times"
+        pending "[PENDING] Wait for 10 seconds"
 
-                sleep 10
-            fi
-        else 
-            pending "[PENDING] Pod is NOT created yet"
-            sleep 10
-        fi
-        
-        tempcount=$((tempcount+1))
-        if [[ $checkcount == $tempcount ]]
-        then
-            error "[ERROR] Pod(s) with label '${pod_label}' is(are) NOT Ready${clear}\n"
-            exit 1
-        fi
-    done
+        sleep 10
+      fi
+    else
+      pending "[PENDING] Pod is NOT created yet"
+      sleep 10
+    fi
+
+    tempcount=$((tempcount + 1))
+    if [[ $checkcount == $tempcount ]]; then
+      error "[ERROR] Pod(s) with label '${pod_label}' is(are) NOT Ready${clear}\n"
+      exit 1
+    fi
+  done
 }
 
 wait_for_pods_ready() {
@@ -132,9 +129,9 @@ wait_for_pods_ready() {
   local wait_counter=0
   local oc_exit_code=0
   local pod_status
-  
+
   while true; do
-    pod_status=$(oc get pods -l $pod_selector -n $pod_namespace -o jsonpath="$JSONPATH") 
+    pod_status=$(oc get pods -l $pod_selector -n $pod_namespace -o jsonpath="$JSONPATH")
     oc_exit_code=$? # capture the exit code instead of failing
 
     if [[ $oc_exit_code -ne 0 ]]; then
@@ -169,11 +166,10 @@ wait_for_pod_name_ready() {
   local pod_namespace=$2
 
   oc status >>/dev/null
-  if [[ $? != 0 ]]
-  then
+  if [[ $? != 0 ]]; then
     error "Cluster is not accessiable"
     echo 1
-    return 
+    return
   fi
 
   echo "Waiting for pod ${pod_name}"
@@ -188,14 +184,13 @@ wait_for_pod_name_ready() {
   fi
 }
 
-function wait_for_just_created_pod_ready(){
+function wait_for_just_created_pod_ready() {
   local namespace=$1
   local wait_counter=0
   local created_pod_name=""
-  
-  while [[ ${created_pod_name} == '' ]]
-  do
-    created_pod_name=$(oc get event -n $namespace|grep 'Started container manager'|grep -E '^[0-9]s'|grep -v -e '^[0-9]m'|awk '{print $4}'|cut -d'/' -f2) 
+
+  while [[ ${created_pod_name} == '' ]]; do
+    created_pod_name=$(oc get event -n $namespace | grep 'Started container manager' | grep -E '^[0-9]s' | grep -v -e '^[0-9]m' | awk '{print $4}' | cut -d'/' -f2)
 
     if [[ $wait_counter -ge 12 ]]; then
       echo
@@ -207,125 +202,118 @@ function wait_for_just_created_pod_ready(){
     wait_counter=$((wait_counter + 1))
     echo "Waiting 5 secs ..."
     sleep 5
-    
+
   done
   info "Catched 'Started container manager' event"
-  
+
   wait_for_pod_name_ready $created_pod_name $namespace
 }
 
-function wait_for_csv_installed(){
-    csv=$1
-    namespace=$2
-    ii=0
-    echo
-    echo "[START] Watching if CSV \"$csv\" is installed" 
-    csv_status=$(oc get csv -n $namespace 2>&1 |grep $csv|awk '{print $NF}')
-    while [[ $csv_status != 'Succeeded' ]]
-    do
-        echo -n "."
-        ((ii=ii+1))
-       if [ $ii -eq 100 ]; then
-            echo "CSV \"$csv\" is NOT installed and it exceeds maximum tries(300s)" 
-            echo "[FAILED] please check the CSV \"$csv\"" 
-            exit 1
-        fi
-        sleep 3
+function wait_for_csv_installed() {
+  csv=$1
+  namespace=$2
+  ii=0
+  echo
+  echo "[START] Watching if CSV \"$csv\" is installed"
+  csv_status=$(oc get csv -n $namespace 2>&1 | grep $csv | awk '{print $NF}')
+  while [[ $csv_status != 'Succeeded' ]]; do
+    echo -n "."
+    ((ii = ii + 1))
+    if [ $ii -eq 100 ]; then
+      echo "CSV \"$csv\" is NOT installed and it exceeds maximum tries(300s)"
+      echo "[FAILED] please check the CSV \"$csv\""
+      exit 1
+    fi
+    sleep 3
 
-        if [ $(expr $ii % 20) == "0" ]; then   
-            echo ""
-            echo "CSV \"$csv\" is NOT installed yet" 
-        fi
+    if [ $(expr $ii % 20) == "0" ]; then
+      echo ""
+      echo "CSV \"$csv\" is NOT installed yet"
+    fi
 
-        csv_status=$(oc get csv -n $namespace 2>&1 |grep $csv|awk '{print $NF}')
-    done
-    echo
-    echo "[END] CSV \"$csv\" is successfully installed" 
+    csv_status=$(oc get csv -n $namespace 2>&1 | grep $csv | awk '{print $NF}')
+  done
+  echo
+  echo "[END] CSV \"$csv\" is successfully installed"
 }
 
 function oc::wait::object::availability() {
-    local cmd=$1 # Command whose output we require
-    local interval=$2 # How many seconds to sleep between tries
-    local iterations=$3 # How many times we attempt to run the command
+  local cmd=$1        # Command whose output we require
+  local interval=$2   # How many seconds to sleep between tries
+  local iterations=$3 # How many times we attempt to run the command
 
-    ii=0
-    echo "[START] Wait for \"${cmd}\" "
-    while [ $ii -le $iterations ]
-    do
+  ii=0
+  echo "[START] Wait for \"${cmd}\" "
+  while [ $ii -le $iterations ]; do
 
-        token=$($cmd &>/dev/null) && returncode=$? || returncode=$?
-        echo "$cmd "|sh
+    token=$($cmd &>/dev/null) && returncode=$? || returncode=$?
+    echo "$cmd " | sh
 
-        if [ $returncode -eq 0 ]; then
-            break
-        fi
+    if [ $returncode -eq 0 ]; then
+      break
+    fi
 
-        ((ii=ii+1))
-        if [ $ii -eq 100 ]; then
-            echo "${cmd} did not return a value$"
-            exit 1
-        fi
-        sleep $interval
-    done
-    echo "[END] \"${cmd}\" is successfully done"
+    ((ii = ii + 1))
+    if [ $ii -eq 100 ]; then
+      echo "${cmd} did not return a value$"
+      exit 1
+    fi
+    sleep $interval
+  done
+  echo "[END] \"${cmd}\" is successfully done"
 }
-
 
 function oc::wait::return::true() {
-    local cmd=$1 # Command whose output we require
-    local interval=$2 # How many seconds to sleep between tries
-    local iterations=$3 # How many times we attempt to run the command
+  local cmd=$1        # Command whose output we require
+  local interval=$2   # How many seconds to sleep between tries
+  local iterations=$3 # How many times we attempt to run the command
 
-    ii=0
-    echo "[START] Wait for \"${cmd}\" "
-    while [ $ii -le $iterations ]
-    do
+  ii=0
+  echo "[START] Wait for \"${cmd}\" "
+  while [ $ii -le $iterations ]; do
 
-        echo "$cmd "|sh
+    echo "$cmd " | sh
 
-        if [ $? -eq 0 ]; then
-            break
-        fi
+    if [ $? -eq 0 ]; then
+      break
+    fi
 
-        ((ii=ii+1))
-        if [ $ii -eq 100 ]; then
-            echo "${cmd} did not return a value$"
-            exit 1
-        fi
-        sleep $interval
-    done
-    echo "[END] \"${cmd}\" return 'true' successfully"
+    ((ii = ii + 1))
+    if [ $ii -eq 100 ]; then
+      echo "${cmd} did not return a value$"
+      exit 1
+    fi
+    sleep $interval
+  done
+  echo "[END] \"${cmd}\" return 'true' successfully"
 }
 
+function get_root_directory() {
+  # Get the directory where this script is located
+  current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-function get_root_directory(){
-# Get the directory where this script is located
-current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  # Traverse up the directory tree to find the .github folder
+  github_dir="$current_dir"
+  while [ ! -d "$github_dir/.git" ] && [ "$github_dir" != "/" ]; do
+    github_dir="$(dirname "$github_dir")"
+  done
 
-# Traverse up the directory tree to find the .github folder
-github_dir="$current_dir"
-while [ ! -d "$github_dir/.git" ] && [ "$github_dir" != "/" ]; do
-  github_dir="$(dirname "$github_dir")"
-done
-
-# If the .github folder is found, set root_directory
-if [ -d "$github_dir/.git" ]; then
-  root_directory="$github_dir"
-  info "The root directory is: $root_directory"
-  echo $root_directory
-else
-  error "Error: Unable to find .github folder."
-fi
+  # If the .github folder is found, set root_directory
+  if [ -d "$github_dir/.git" ]; then
+    root_directory="$github_dir"
+    info "The root directory is: $root_directory"
+    echo $root_directory
+  else
+    error "Error: Unable to find .github folder."
+  fi
 }
 
-function is_positive(){
+function is_positive() {
   input_val=$1
 
-  if [[ $input_val == '0' ||  $input_val == "true" || $input_val == 'True' || $input_val == 'yes' || $input_val == 'Yes' || $input_val == 'y' || $input_val == 'Y' ]]
-  then
+  if [[ $input_val == '0' || $input_val == "true" || $input_val == 'True' || $input_val == 'yes' || $input_val == 'Yes' || $input_val == 'y' || $input_val == 'Y' ]]; then
     echo 0
-  elif [[ $input_val == '1' || $input_val == 'false' || $input_val == 'False' || $input_val == 'no' || $input_val == 'No' || $input_val == 'n' || $input_val == 'N' ]]
-  then
+  elif [[ $input_val == '1' || $input_val == 'false' || $input_val == 'False' || $input_val == 'no' || $input_val == 'No' || $input_val == 'n' || $input_val == 'N' ]]; then
     echo 1
   else
     die "Input value(${input_val}) is not allowed text to check positive or not"
@@ -333,61 +321,100 @@ function is_positive(){
   fi
 }
 
-
-function check_if_result(){
+function check_if_result() {
   rc=$1
-  if [[ $? != 0 ]]
-  then 
+  if [[ $? != 0 ]]; then
     die "FAIL"
-  else  
+  else
     success "PASS"
   fi
 }
 
-function check_oc_status(){
-    info "[INFO] Checking oc connection and user role"
-    userName=$(oc whoami) && connected=$? || connected=$?
-    
-    if [[ ${connected} -eq '0' ]]; then     
-      groupNames=$(oc get group |grep ${userName}|awk '{print $1}')
+function check_oc_status() {
+  info "[INFO] Checking oc connection and user role"
+  userName=$(oc whoami) && connected=$? || connected=$?
 
-      for groupName in $groupNames
-      do
-          clusterAdmin=$(oc get clusterrolebindings -o json | jq '.items[] | select(.metadata.name| startswith("cluster-admin")) | .subjects[].name'|egrep "$userName|$groupName" |wc -l)
-          if (( ${clusterAdmin} >= 1 ))
-          then
-              success "[PASS] You logged to the cluster as a cluster-admin"
-              break
-          else
-              error "[FAIL] You logged to the cluster but you are not cluster-admin"
-              light_info "        Please log in to your cluster as a cluster-admin"
-              exit 1
-          fi
-      done
-    else
-        error "[FAIL] You are NOT logged to the cluster. Please log in to your cluster"
+  if [[ ${connected} -eq '0' ]]; then
+    groupNames=$(oc get group | grep ${userName} | awk '{print $1}')
+
+    for groupName in $groupNames; do
+      clusterAdmin=$(oc get clusterrolebindings -o json | jq '.items[] | select(.metadata.name| startswith("cluster-admin")) | .subjects[].name' | egrep "$userName|$groupName" | wc -l)
+      if ((${clusterAdmin} >= 1)); then
+        success "[PASS] You logged to the cluster as a cluster-admin"
+        break
+      else
+        error "[FAIL] You logged to the cluster but you are not cluster-admin"
+        light_info "        Please log in to your cluster as a cluster-admin"
         exit 1
-    fi
+      fi
+    done
+  else
+    error "[FAIL] You are NOT logged to the cluster. Please log in to your cluster"
+    exit 1
+  fi
 }
 
 retry_if_http_code_is_not_200() {
-    local retries=$1
-    local interval=$2
-    local result_var=$3
-    shift 3
-    local cmd=("$@")
-    local result
+  local retries=$1
+  local interval=$2
+  local result_var=$3
+  shift 3
+  local cmd=("$@")
+  local result
 
-    for ((i=0; i<retries; i++)); do
-        result=$("${cmd[@]}")
-        if [[ $result == "200" ]]; then
-            eval "$result_var=\"$result\""
-            return 0
-        else
-            info "return code is not 200. retry"
-            sleep $interval
-        fi
-    done
-    eval "$result_var=\"$result\""
-    return 1
+  for ((i = 0; i < retries; i++)); do
+    result=$("${cmd[@]}")
+    if [[ $result == "200" ]]; then
+      eval "$result_var=\"$result\""
+      return 0
+    else
+      info "return code is not 200. retry"
+      sleep $interval
+    fi
+  done
+  eval "$result_var=\"$result\""
+  return 1
+}
+
+function check_rosa_access() {
+  info "[INFO] Checking ROSA access"
+  # 1. does ~/.aws folder exist?
+  if [ ! -d "$HOME/.aws" ]; then
+    error "[FAIL] ~/.aws directory does not exist."
+    exit 1
+  fi
+
+  # 2. does credentials file exist?
+  if [ ! -f "$HOME/.aws/credentials" ]; then
+    error "[FAIL]: credentials file does not exist in ~/.aws."
+    exit 1
+  fi
+
+  # 3. does config file exist?
+  if [ ! -f "$HOME/.aws/config" ]; then
+    # if config does not exist, does AWS_REGION env parameter set?
+    if [ -z "$AWS_REGION" ]; then
+      error "[FAIL]: Neither config file exists in ~/.aws nor AWS_REGION environment variable is set."
+      exit 1
+    fi
+  fi
+
+  # 4. can rosa login?
+  rosa login --token=$OCM_TOKEN
+
+  if [[ $? != "0" ]]; then
+    error "[FAIL] rosa login failed"
+    exit 1
+  fi
+
+  # 5. can rosa list cluster?
+  rosa list clusters > /dev/null
+
+  if [[ $? != "0" ]]; then
+    error "[FAIL] rosa can not list cluster. It would fail to create a new cluster"
+    error "       please check region and permission"
+    exit 1
+  fi
+
+  success "[SUCCESS] All checks passed!"
 }
---
+++ src/roles/openshift/rosa-install/main.sh
@@ -0,0 +1,185 @@
+#!/usr/bin/env bash
+## INIT START ##
+if [[ $DEBUG == "0" ]]; then
+  set -x
+fi
+
+# Get the directory where this script is located
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Traverse up the directory tree to find the .github folder
+github_dir="$current_dir"
+while [ ! -d "$github_dir/.git" ] && [ "$github_dir" != "/" ]; do
+  github_dir="$(dirname "$github_dir")"
+done
+
+# If the .github folder is found, set root_directory
+if [ -d "$github_dir/.git" ]; then
+  root_directory="$github_dir"
+  echo "The root directory is: $root_directory"
+else
+  echo "Error: Unable to find .github folder."
+fi
+## INIT END ##
+
+#################################################################
+source $root_directory/commons/scripts/utils.sh
+index_role_name=$(basename $ROLE_DIR)
+role_name=$(yq e '.role.name' ${current_dir}/config.yaml)
+result=1
+
+check_rosa_access
+
+rosa create cluster --sts --oidc-config-id $OIDC_CONFIG_ID --cluster-name=$CLUSTER_NAME --replicas=$WORKER_NODE_COUNT --sts --mode=auto --hosted-cp --subnet-ids=$PRIVATE_SUBNET,$PUBLIC_SUBNET --compute-machine-type $MACHINE_POOL_TYPE --role-arn=$INSTALLER_ROLE --support-role-arn=$SUPPORT_ROLE --worker-iam-role=$WORKER_ROLE
+
+if [[ $? != "0" ]]; then
+  error "[FAIL] ROSA failed to create a cluster"
+  echo "${index_role_name}::1" >>${REPORT_FILE}
+  exit 1
+else   
+  success "[SUCCESS] The ROSA cluster has been created. Please wait until it's ready!"
+fi
+
+# Function to check the cluster status
+check_cluster_status() {
+  rosa describe cluster -c "$CLUSTER_NAME" --output json | jq -r '.status.state'
+}
+
+# Periodically check the cluster status. (60m)
+# Retry logic variables
+MAX_RETRIES=60
+RETRY_INTERVAL=60 # Interval between retries in seconds
+RETRY_COUNT=0
+
+# Infinite loop to check the cluster status
+while true; do
+  STATUS=$(check_cluster_status)
+  RETRY_COUNT=$((RETRY_COUNT + 1))
+
+  info "[INFO] Attempt ${RETRY_COUNT} of $MAX_RETRIES."
+
+  case "$STATUS" in
+  "ready")
+    success "[SUCCESS] The ROSA cluster is now ready!"
+    result=0
+    break
+    ;;
+  "error")
+    error "[FAIL] Cluster status: $STATUS."
+    rosa describe cluster -c "$CLUSTER_NAME" --output json
+    echo "${index_role_name}::1" >>${REPORT_FILE}
+    exit 1
+    ;;
+  *)
+    info "[INFO] Cluster status: $STATUS. Waiting for it to become ready..."
+    ;;
+  esac
+
+  if ((RETRY_COUNT >= MAX_RETRIES)); then
+    error "[FAIL] Cluster failed to be created after $MAX_RETRIES attempts."
+    echo "${index_role_name}::1" >>${REPORT_FILE}
+    exit 1
+  fi
+
+  sleep $RETRY_INTERVAL
+done
+
+info "[INFO] Add a openshift user($OCP_ADMIN_ID)"
+rosa create idp --cluster=$CLUSTER_NAME --type=htpasswd -u $OCP_ADMIN_ID:$OCP_ADMIN_PW --name htpasswd
+if [[ $? != 0 ]]; then
+  error "[FAIL] Failed to create a default user"
+  exit 1
+fi
+
+info "[INFO] add cluster-admin role to the user($OCP_ADMIN_ID)"
+rosa grant user cluster-admin --user=$OCP_ADMIN_ID --cluster=$CLUSTER_NAME
+if [[ $? != 0 ]]; then
+  error "[FAIL] Failed to grant cluster-admin role to the default user"
+  exit 1
+fi
+sleep 10
+IsUserAdded=$(rosa list users --cluster=$CLUSTER_NAME | grep $OCP_ADMIN_ID | grep cluster-admin | wc -l)
+
+if [[ $IsUserAdded != 1 ]]; then
+  error "[FAIL] Openshift cluster user failed to be added"
+  rosa list users --cluster=$CLUSTER_NAME
+  echo "${index_role_name}::1" >>${REPORT_FILE}
+  exit 1
+else 
+  success "[SUCCESS] The OpenShift cluster-admin user has been successfully added."
+fi
+
+############# VERIFY #############
+# check if cluster is created
+# check if user is added with right permission
+
+# Retry logic variables
+MAX_RETRIES=30
+RETRY_INTERVAL=60 # Interval between retries in seconds
+RETRY_COUNT=0
+
+# Function to get cluster info as JSON
+check_cluster_json() {
+  rosa describe cluster -c "$CLUSTER_NAME" --output json
+}
+
+# Infinite loop to check the cluster status
+info "[INFO] Verifying if the cluster has been created and the user has been added"
+while true; do
+  # Fetch cluster information
+  cluster_info_json=$(check_cluster_json)  
+  RETRY_COUNT=$((RETRY_COUNT + 1))
+
+  info "[INFO] Attempt ${RETRY_COUNT} of $MAX_RETRIES."
+
+  # Check if the retry count has exceeded the maximum limit
+  if ((RETRY_COUNT > MAX_RETRIES)); then
+    error "[FAIL] Failed to create the cluster after $MAX_RETRIES attempts."
+    echo "${index_role_name}::1" >>"${REPORT_FILE}"
+    break
+  fi
+  # Extract URLs from the JSON
+  cluster_console_url=$(echo "$cluster_info_json" | jq -r '.console.url')
+  cluster_api_url=$(echo "$cluster_info_json" | jq -r '.api.url')
+
+  # Check if the console URL is present
+  if [[ -z "$cluster_console_url" ]]; then
+    error "[ERROR] Console URL not available yet. Waiting..."
+    ((RETRY_COUNT++))
+  else
+    info "[INFO] Cluster console URL: $cluster_console_url"
+
+    # Check if the API URL is present
+    if [[ $cluster_console_url == null ]]; then
+      info "[INFO] API URL not available yet. Waiting..."
+    else
+      info "[INFO] Cluster API URL: $cluster_api_url"
+
+      # Attempt to login to the cluster
+      info "[INFO] oc login -u $OCP_ADMIN_ID -p XXX --server $cluster_api_url --insecure-skip-tls-verify=true"
+      oc login -u "$OCP_ADMIN_ID" -p "$OCP_ADMIN_PW" --server "$cluster_api_url" --insecure-skip-tls-verify=true
+      oc_logined=$?
+      if [[ $oc_logined -eq 0 ]]; then
+        cluster_token=$(oc whoami -t)
+        success "[SUCCESS] Loopy has verified that the cluster ($CLUSTER_NAME) has been successfully created and the user ($OCP_ADMIN_ID) has been added"
+        result=0
+        break # Exit the loop since the cluster is successfully created
+      else
+        error "[ERROR] Login to OpenShift failed. Retrying..."
+      fi
+    fi
+  fi
+  
+  sleep "$RETRY_INTERVAL"
+done
+
+############# OUTPUT #############
+echo "CLUSTER_CONSOLE_URL=${cluster_console_url}" >>${OUTPUT_ENV_FILE}
+echo "CLUSTER_API_URL=${cluster_api_url}" >>${OUTPUT_ENV_FILE}
+echo "CLUSTER_ADMIN_ID=${OCP_ADMIN_ID}" >>${OUTPUT_ENV_FILE}
+echo "CLUSTER_ADMIN_PW=${OCP_ADMIN_PW}" >>${OUTPUT_ENV_FILE}
+echo "CLUSTER_TOKEN=${cluster_token}" >>${OUTPUT_ENV_FILE}
+echo "CLUSTER_TYPE=${CLUSTER_TYPE}" >>${OUTPUT_ENV_FILE}
+
+############# REPORT #############
+echo "${index_role_name}::${result}" >>${REPORT_FILE}
